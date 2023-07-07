de2bbfbb2028079eed5b4c943a6cd71313ae7c07
Yasya <yasya.aslina@gopay.co.id>
2023-07-07 11:19:45 +0700

    feat: add new async.js

---
+++ async.js
@@ -0,0 +1,33 @@
+const wait = time => new Promise(resolve => setTimeout(resolve, time));
+
+// Asynchronous function that fetches data from the JSONPlaceholder API
+async function fetchData() {
+    try {
+      const response =  await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await response.json();
+  
+      await wait(3000)
+  
+      return data; // Return the data as a resolved Promise
+    } catch (error) {
+      throw error; // Throw the error to reject the Promise
+    }
+  }
+  
+// Calling the async function
+async function main() {
+    try {
+        //nggak ada await
+        const result = await fetchData();
+        console.log('Result:', result);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Call the main function
+main();
+
+//1. Hasilnya promise (gk berhasil dan gk ggl jg)
+//2. kemungkinan berhasil (result json) 
+//3. berhasil tp ditunggu dulu 3 detik
